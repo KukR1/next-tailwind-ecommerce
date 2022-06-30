397d4c99e7e969694ee639527ddac008d387a9ad
KukR1 <nikdani123@hotmail.com>
2022-06-30 09:54:38 +0300

    Uploading project

---
+++ components/Layout.js
@@ -0,0 +1,47 @@
+import { useEffect, useState, useContext } from 'react';
+import Head from 'next/head';
+import Link from 'next/link';
+import { Store } from '../utils/Store';
+
+export const Layout = ({ title, children }) => {
+  const { state, dispatch } = useContext(Store);
+  const { cart } = state;
+  const [cartItemsCount, setCartItemsCount] = useState(0);
+  useEffect(() => {
+    setCartItemsCount(cart.cartItems.reduce((a, c) => a + c.quantity, 0));
+  }, [cart.cartItems]);
+  return (
+    <>
+      <Head>
+        <title>{title ? title + ' - E-Shop' : 'E-Shop'}</title>
+        <meta name="description" content="An e-shop" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="flex min-h-screen flex-col justify-between">
+        <header>
+          <nav className="flex h-12 items-center px-4 justify-between shadow-md">
+            <Link href="/">
+              <a className="text-lg font-bold">E-Shop</a>
+            </Link>
+            <div>
+              <Link href="/cart">
+                <a className="p-2">
+                  Cart
+                  {cartItemsCount > 0 && (
+                    <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
+                      {cartItemsCount}
+                    </span>
+                  )}
+                </a>
+              </Link>
+            </div>
+          </nav>
+        </header>
+        <main className="container m-auto mt-4 px-4">{children}</main>
+        <footer className="flex h-10 justify-center items-center shadow-inner">
+          <p>Copyright © 2022 E-Shop</p>
+        </footer>
+      </div>
+    </>
+  );
+};
---
+++ components/ProductItem.js
@@ -0,0 +1,53 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useContext } from 'react';
+import { Store } from '../utils/Store';
+
+const ProductItem = ({ product }) => {
+  const { state, dispatch } = useContext(Store);
+  const router = useRouter();
+
+  const addToCartHandler = () => {
+    const existItem = state.cart.cartItems.find((x) => x.slug === product.slug);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+
+    if (product.countInStock < quantity) {
+      alert('Sorry. Product is out of stock');
+      return;
+    }
+
+    dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
+    router.push('/cart');
+  };
+  return (
+    <div className="card pb-3">
+      <Link href={`/product/${product.slug}`}>
+        <a>
+          <img
+            src={product.image}
+            alt={product.name}
+            className="rounded shadow"
+          />
+        </a>
+      </Link>
+      <div className="flex flex-col items-center justify-center">
+        <Link href={`/product/${product.slug}`}>
+          <a>
+            <h2 className="text-lg">{product.name}</h2>
+          </a>
+        </Link>
+        <p className="mb-2">{product.brand}</p>
+        <p>${product.price}</p>
+        <button
+          className="primary-button"
+          type="button"
+          onClick={addToCartHandler}
+        >
+          Add to cart
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ProductItem;
